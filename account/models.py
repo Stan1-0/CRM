e71acba9d79c43cd24e7031dffef65c38b103b43
Stan1-0 <stankofb@gmail.com>
2024-04-26 08:27:47 +0000

    "Replace CharField with PhoneNumberField and EmailField for phone and email fields in Customer model"

--- account/models.py
+++ account/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    name =models.CharField(max_length=200, null=True)
-    phone =models.CharField(max_length=200, null=True)
-    email =models.CharField(max_length=200, null=True)
-    date_created =models.DateTimeField(auto_now_add=True, null=True)
-    
+    name = models.CharField(max_length=200, null=True)
+    phone_number_field = models.PhoneNumberField(max_length=200, null=True)
+    email_field = models.EmailField(max_length=200, null=True)
+    date_created = models.DateTimeField(max_length=200, null=True)
