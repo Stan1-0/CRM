c086ea4f9c555e8fcf1ef08e23a6b36fa1a2cca6
Stan1-0 <stankofb@gmail.com>
2024-04-26 08:35:54 +0000

    Updated INSTALLED\_APPS to include 'account' Pyc files in CRM

--- account/models.py
+++ account/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
-    phone_number_field = models.PhoneNumberField(max_length=200, null=True)
-    email_field = models.EmailField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null= True)
+    email = models.EmailField(max_length=200, null=True)
     date_created = models.DateTimeField(max_length=200, null=True)
