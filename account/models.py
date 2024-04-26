b23d671865a8d0883a70a1c8320c98482ff95c2c
Stan1-0 <stankofb@gmail.com>
2024-04-26 08:56:46 +0000

    This commit adds three new models to the account app: Customer, Product, and Order.

--- account/models.py
+++ account/models.py
@@ -6,3 +6,27 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null= True)
     email = models.EmailField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
