c8bac2e9d36638b364c22ecea417070c793e034d
Stan1-0 <stankofb@gmail.com>
2024-04-26 09:25:31 +0000

    Add __str__ method to Customer model and remove unnecessary method from same model

--- account/models.py
+++ account/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 # Create your models here.
+def __str__(self):
+        return self.name
+    
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null= True)
     email = models.EmailField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    
-    def __str__(self):
-        return self.name
-    
+    date_created = models.DateTimeField(auto_now_add=True, null=True)    
 
 class Product(models.Model):
     CATEGORY = (
