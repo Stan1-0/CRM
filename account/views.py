65124e16b1798b5384ee7a14a08ec77afc3e5551
Stan1-0 <stankofb@gmail.com>
2024-05-11 22:22:00 +0000

    "Updated createOrder view: increased OrderFormSet extra limit to 10 and set initial queryset to none"

--- account/views.py
+++ account/views.py
@@ -28,9 +28,9 @@ def customer(request, pk):
     return render(request, 'customer.html', context)
 
 def createOrder(request, pk):
-    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'))
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
     customer = Customer.objects.get(id=pk)
-    formset = OrderFormSet(instance=customer)
+    formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
     #form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
         #print('printing POST:', request.POST)
